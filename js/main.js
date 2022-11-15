039cc108a77a5f14945788972cbe31ad62ac9c57
ubva <aubaydullayevaa4@gmail.com>
2022-11-15 22:15:28 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let value = prompt("summani kiriting")
+let word = document.querySelector(".word")
+let result = value;
+
+if(result >= 8317000){
+    alert("Oq yo'l Alisher")
+}
+else{
+    alert("Alisher, ozgina sabr qilish kerak bo'lar ekan")
+}
