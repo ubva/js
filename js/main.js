97fd1565bf83d3e89c747bfea5fd0f14bda98eb2
ubva <aubaydullayevaa4@gmail.com>
2022-11-16 09:36:49 +0500

    Add to js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let value = prompt("summani kiriting")
 let word = document.querySelector(".word")
 let result = value;
+let ticket = 4716000;
+let hotel = 2358000;
+let services = 1242000;
+
 
 if(result >= 8317000){
     alert("Oq yo'l Alisher")
